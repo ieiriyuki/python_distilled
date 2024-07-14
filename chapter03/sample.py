07ec4baa015fe666ccf2faa255042cf8cf2502b4
ieiriyuki <ieiriyuki@exam.com>
2024-07-14 14:24:24 +0900

    feat: user defined context sample

--- chapter03/sample.py
+++ chapter03/sample.py
@@ -1,3 +1,6 @@
+import traceback
+
+
 def some():
     try:
         return 1 / 0
@@ -6,3 +9,26 @@ def some():
 
 
 some()
+
+
+class MyContext:
+    def __enter__(self):
+        print('enter')
+        return self
+
+    def __exit__(self, type, val, tb):
+        print('exit')
+        if type is None:
+            print("success")
+
+        print(type, val)
+        print("".join(traceback.format_tb(tb)))
+        return True
+
+
+with MyContext() as c:
+    print('inside')
+
+with MyContext() as c:
+    print('error')
+    raise Exception('foo')
